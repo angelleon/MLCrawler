3070b80cc0598be921127e726f2a3eef1d5dc7db
Angel Leon <luianglenlop@gmail.com>
2022-05-08 19:35:04 -0500

    defining test for load_categories

--- src/test/test_mlcrawler.py
+++ src/test/test_mlcrawler.py
@@ -3,10 +3,19 @@ import unittest
 from crawler.crwlr import load_categories
 
 class CrawlerTest(unittest.TestCase):
+    def create_file(self, path: str, content: list[str]):
+        with open(path, 'w') as f:
+            f.writelines(content)
+
     def test_load_categories(self):
-        expected = ['']
+        content = ['http://mercadolibre.com.mx']
+        expected = content[:]
         categories_path = 'test_categories.txt'
-        with open(categories_path, 'w') as f:
-            f.writelines(expected)
+        self.create_file(categories_path, expected)
         actual = load_categories(categories_path)
         self.assertListEqual(expected, actual)
+        content = ['badurl : // example . com', 'nourl dot com']
+        self.create_file(categories_path, content)
+        with self.assertRaises(ValueError):
+            load_categories(categories_path)
+
