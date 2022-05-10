a34a4fb5367cb617f8ab98b7e265f97e3d9cd47d
Angel Leon <luianglenlop@gmail.com>
2022-05-10 03:16:21 -0500

    fixing import

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 
-from . import crwlr
+import crwlr
 
 
 def main():
@@ -9,6 +9,7 @@ def main():
     parser.add_argument('-c', '--categories', help='file containing categories, one per line', default='categories.txt')
     args = parser.parse_args()
     categories = args.categories
+    args = tuple()
     crwlr.start(categories, *args, **kwargs)
 
 
