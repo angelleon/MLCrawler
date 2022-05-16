c0af6f2b228cbf8a87c0f92ccd635449e8fc3d8a
Angel Leon <luianglenlop@gmail.com>
2022-05-16 04:08:04 -0500

    adding cli options

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -4,13 +4,23 @@ import crwlr
 
 
 def main():
-    kwargs = dict()
     parser = ArgumentParser()
     parser.add_argument('-c', '--categories', help='file containing categories, one per line', default='categories.txt')
+    parser.add_argument('-p', '--pages', help='number search of pages to fetch and analyze', type=int, default=10)
+    parser.add_argument('-o', '--output', help='file to write the results on', default='results.csv')
+    parser.add_argument('-n', '--num-procs', help='Number of processes', type=int, default=5)
+    parser.add_argument('-t', '--timeout-q', help='Timeout when fetching from queues, in seconds', type=float, default=0.01)
+    parser.add_argument
     args = parser.parse_args()
     categories = args.categories
+    pages = args.pages
+    output = args.output
+    num_procs = args.num_procs
+    q_timeout = args.timeout_q
+    print(repr(q_timeout))
     args = tuple()
-    crwlr.start(categories, *args, **kwargs)
+    kwargs = dict()
+    crwlr.start(categories, pages, num_procs, output, q_timeout, *args, **kwargs)
 
 
 
