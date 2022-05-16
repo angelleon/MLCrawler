3d3082bd7d4f16b41b3023534dc785bcdf3a69e9
Angel Leon <luianglenlop@gmail.com>
2022-05-16 09:50:37 -0500

    adding result saving

--- src/crawler/crwlr/crwlr.py
+++ src/crawler/crwlr/crwlr.py
@@ -10,27 +10,31 @@ from queue import Empty as QEmpty
 from dataclasses import dataclass
 from enum import Enum
 from time import sleep
+from io import TextIOWrapper
+from json import dump
 
 import requests
-from bs4 import BeautifulSoup as Bs
+from bs4 import BeautifulSoup as Bs, Tag
 
 basicConfig(
     level=DEBUG,
-    #level=ERROR
+    # level=ERROR
     format="[%(levelname)-8.8s]:\t[%(name)-10.10s]:%(funcName)-8.8s:%(lineno)4d:\t%(message)s"
 )
 log = getLogger(__name__)
 
 modname = __name__
 
+
 class CrwlrFilter(Filter):
     def filter(self, record):
         return record.name == modname
 
-filter = CrwlrFilter()
+
+log_filter = CrwlrFilter()
 
 for handler in logging_root.handlers:
-        handler.addFilter(filter)
+    handler.addFilter(log_filter)
 
 
 items_per_page = 48
@@ -57,6 +61,13 @@ class PageType(Enum):
     SEARCH_PAGE = 1
     PRODUCT_PAGE = 2
 
+
+price_regex = re.compile(r'\d+(\.\d\d)?')
+old_price_regex = re.compile(
+    r'"andes-visually-hidden">(Precio anterior: )?\d+(\.\d\d)? pesos')
+brand_regex = re.compile(r'Marca.*</span>')
+brand_left_wrapper_regex = re.compile(r'<span.*>')
+
 # TODO: add behavior to this class
 
 
@@ -76,7 +87,22 @@ class CategoryStatus:
     completed_search_pages: int = 0
     total_products: int = 0
     completed_products:  int = 0
+    saved_products: int = 0
     base_url: str = ''
+
+
+@dataclass()
+class ProductInfo:
+    description: str = ''
+    price: str = ''
+    old_price: str = ''
+    brand: str = ''
+    image_url: str = ''
+    base_url: str = ''
+
+
+def serialize_dataclass(obj):
+    return {k: v for k, v in obj.__dict__.items() if k != 'base_url'}
 
 
 """https://developers.whatismybrowser.com/useragents/explore/"""
@@ -102,66 +128,106 @@ PRODUCT_PAGE = 1
 
 
 # TODO: improve error handling
-# TODO: avoid passing unnecesary arguments, maybe use dict expansion
-def extractor(page: Page) -> list[Page]:
-    #log.debug(f'Extracting page {page}')
+def extract_search(page: Page, document_tree: Bs):
     page_content = []
-    document_tree = Bs(page.response.text, 'lxml')
-    # TODO: complete definition here
-    # TODO: refactor these conditions, look for an adequate design pattern
-    if page.page_type == PageType.SEARCH_PAGE:
-        #log.debug(f'Extracting search {page=}')
-        page_number = document_tree.find(
-            class_='andes-pagination__button andes-pagination__button--current')
-        has_single_result_page = page_number is None
-        if has_single_result_page:
-            page_number = 1
-        else:
-            page_number = int(page_number.find(
-                class_='andes-pagination__link').text)
-        results_container: Bs = document_tree.find_all(
-            class_='ui-search-results')[0]
-        log.debug(f'Extracting from contianer {repr(results_container)[:100]}')
-        item_containers = results_container.find_all(
-            class_='ui-search-layout__item')
-        log.debug(f'Item containers {len(item_containers)}')
-        item_count = 0
-        for item_container in item_containers:
-            a = item_container.find(class_='ui-search-link')
-            url = a.attrs['href']
-            product_page = Page(
-                page.base_url, url=url, page_type=PageType.PRODUCT_PAGE, page_number=page_number)
-            page_content.append(product_page)
-            item_count += 1
-        log.debug(f'Extracted {item_count} products from search page')
-        # a tag
-        # FIXME: check when this is not present
-        next_button = document_tree.find_all(
-            class_='andes-pagination__link ui-search-link')
-        next_button = next_button[1 if len(next_button) == 2 else 0]
-        url = next_button.attrs['href']
-        # TODO: avoid innecesary data duplication
-        search_page = Page(base_url=page.base_url, url=url,
-                           page_type=PageType.SEARCH_PAGE, page_number=page_number+1)
-        page_content.append(
-            search_page)
-        return page_content
-    elif page.page_type == PageType.PRODUCT_PAGE:
-        #log.debug(f'Extracting product {page=}')
+    #log.debug(f'Extracting search {page=}')
+    page_number = document_tree.find(
+        class_='andes-pagination__button andes-pagination__button--current')
+    has_single_result_page = page_number is None
+    if has_single_result_page:
+        page_number = 1
+    else:
+        page_number = int(page_number.find(
+            class_='andes-pagination__link').text)
+    results_container: Bs = document_tree.find_all(
+        class_='ui-search-results')[0]
+    log.debug(f'Extracting from contianer {repr(results_container)[:100]}')
+    item_containers = results_container.find_all(
+        class_='ui-search-layout__item')
+    log.debug(f'Item containers {len(item_containers)}')
+    item_count = 0
+    for item_container in item_containers:
+        a = item_container.find(class_='ui-search-link')
+        url = a.attrs['href']
+        product_page = Page(
+            page.base_url, url=url, page_type=PageType.PRODUCT_PAGE, page_number=page_number)
+        page_content.append(product_page)
+        item_count += 1
+    log.debug(f'Extracted {item_count} products from search page')
+    # a tag
+    # FIXME: check when this is not present
+    next_button = document_tree.find_all(
+        class_='andes-pagination__link ui-search-link')
+    next_button = next_button[1 if len(next_button) == 2 else 0]
+    url = next_button.attrs['href']
+    # TODO: avoid innecesary data duplication
+    search_page = Page(base_url=page.base_url, url=url,
+                       page_type=PageType.SEARCH_PAGE, page_number=page_number+1)
+    page_content.append(
+        search_page)
+    return page_content
+
+
+# TODO: improve error handling
+def extract_product(document_tree: Bs) -> ProductInfo:
+    # extract description
+    description_container: Tag = document_tree.find(
+        class_='ui-pdp-description__content')
+    description = ''.join(c for c in filter(
+        lambda x: not isinstance(x, Tag), description_container.contents))
+    log.debug(f'Processed description {description}')
+    # extract price
+    #price_container = document_tree.find('span', class_='andes-money-amount ui-pdp-price__part andes-money-amount--cents-superscript andes-money-amount--compact')
+    price_container: Tag = document_tree.find(itemprop='price')
+    price_container_html = str(price_container)
+    log.debug(f'price container html {price_container_html}')
+    price_match = price_regex.search(price_container_html)
+    if price_match is None:
         pass
+    price = price_container_html[price_match.start():price_match.end()]
+    log.debug(f'{price=}')
+    # extract Old price
+    old_price_container = document_tree.find(class_='ui-pdp-price')
+    old_price_container_html = str(old_price_container)
+    old_price_match = old_price_regex.search(old_price_container_html)
+    if old_price_match is None:
+        log.debug(f'{old_price_container_html=}')
+    old_price_container = old_price_container_html[old_price_match.start(
+    ):old_price_match.end()]
+    price_match = price_regex.search(old_price_container)
+    old_price = old_price_container[price_match.start():price_match.end()]
+    # Extract brand
+    # FIXME: this is so ugly and ineficient, won't suggest this for a technical test, there are better ways to test the knowlege in regex
+    # TODO: figure out a better way to jistify the use of regex
+    brand_container = document_tree.find(
+        class_='ui-vpp-highlighted-specs__striped-specs')
+    brand_container_html = str(brand_container)
+    brand_match = brand_regex.search(brand_container_html)
+    brand_container_html = brand_container_html[brand_match.start(
+    ):brand_match.end()]
+    brand_left_wrapper_match = brand_left_wrapper_regex.search(
+        brand_container_html)
+    brand_container_html = brand_container_html[brand_left_wrapper_match.end(
+    ):]
+    brand = brand_container_html.replace('</span>', '')
+    # Extract image url
+    img_container = document_tree.find(
+        class_='ui-pdp-image ui-pdp-gallery__figure__image')
+    image_url = img_container.attrs['src']
+    product_info = ProductInfo(description=description, price=price,
+                               old_price=old_price, image_url=image_url, brand=brand)
+    log.debug(f'Extracted product {product_info}')
+    return product_info
 
 
-def fetcher(url_queue: Queue, response_queue: Queue, stop_ev: Event, category_fetch_completed: Event, q_timeout: float):
+def fetcher(url_queue: Queue, response_queue: Queue, stop_ev: Event, q_timeout: float):
     while not stop_ev.is_set():
         #log.debug('Executing fetcher loop')
         try:
-            #print(repr(q_timeout), type(q_timeout))
+            #log.debug(repr(q_timeout), type(q_timeout))
             page: Page = url_queue.get(timeout=q_timeout)
             log.debug(page)
-        except QEmpty as ex:
-            #log.debug('Continuing with next iteration')
-            if category_fetch_completed.is_set():
-                break
+        except QEmpty:
             continue
         #user_agent = choice(user_agents)
         user_agent = user_agents[2]  # we're chrome on win10
@@ -187,8 +253,25 @@ def fetcher(url_queue: Queue, response_queue: Queue, stop_ev: Event, category_fe
     log.debug('Exitting fetcher loop')
 
 
-def save_product_info(*args, **kwargs):
-    pass
+# TODO: implement this in better way
+def save_product_info(storage_queue: Queue, product_info_file: TextIOWrapper, storage_lock: Lock, stop_ev: Event, q_timeout: float, category_status: dict[CategoryStatus], data_lock: Lock):
+    while not stop_ev.is_set():
+        try:
+            product_info: ProductInfo = storage_queue.get(timeout=q_timeout)
+        except QEmpty:
+            continue
+        log.debug(('=' * 50) + 'Saving details')
+        storage_lock.acquire()
+        dump(serialize_dataclass(product_info), product_info_file)
+        product_info_file.write(',')
+        product_info_file.flush()
+        storage_lock.release()
+        log.debug(('=' * 50) + 'Saved details')
+        data_lock.acquire()
+        category: CategoryStatus = category_status[product_info.base_url]
+        category.saved_products += 1
+        category_status[product_info.base_url] = category
+        data_lock.release()
 
 
 def save_product_link(*args, **kwargs):
@@ -196,26 +279,23 @@ def save_product_link(*args, **kwargs):
 
 
 # TODO: improve function signature
-def processor(response_queue: Queue, url_queue: Queue, q_timeout: float, stop_env: Event, category_status: dict, category_fetch_completed: Event, data_lock: Lock):
+def processor(response_queue: Queue, url_queue: Queue, q_timeout: float, stop_env: Event, category_status: dict, data_lock: Lock, storage_q: Queue):
     product_count = 0
     while not stop_env.is_set():
         try:
-            #print(repr(q_timeout))
             page: Page = response_queue.get(timeout=q_timeout)
-        except QEmpty as ex:
-            # log.debug(ex)
-            # log.debug(type(ex))
+        except QEmpty:
             continue
-        category: CategoryStatus = category_status[page.base_url]
-        extracted_pages = extractor(page)
-        #log.debug(f'{extracted_pages=}')
+        document_tree = Bs(page.response.text, 'lxml')
         if page.page_type == PageType.SEARCH_PAGE:
-            print('Awaiting for data lock')
+            extracted_info = extract_search(page, document_tree)
+            log.debug('Awaiting for data lock')
             data_lock.acquire()
-            print('Data lock acquirred')
-            for extracted_page in extracted_pages:
+            category: CategoryStatus = category_status[page.base_url]
+            log.debug('Data lock acquirred')
+            for extracted_page in extracted_info:
                 if extracted_page.page_type == PageType.SEARCH_PAGE and extracted_page.page_number <= category.total_search_pages:
-                    print(f'Adding search page {extracted_page}')
+                    log.debug(f'Adding search page {extracted_page}')
                     url_queue.put(extracted_page)
                 elif extracted_page.page_type == PageType.PRODUCT_PAGE:
                     save_product_link(extracted_page)
@@ -224,17 +304,20 @@ def processor(response_queue: Queue, url_queue: Queue, q_timeout: float, stop_en
                     url_queue.put(extracted_page)
             category.completed_search_pages += 1
             category_status[page.base_url] = category
-            print(f'SP {category_status}')
+            log.debug(f'SP {category_status}')
             data_lock.release()
         elif page.page_type == PageType.PRODUCT_PAGE:
-            save_product_info(extracted_pages)
-            data_lock.acquire()
-            category.completed_products += 1
-            category_status[page.base_url] = category
-            data_lock.release()
+            extracted_info: ProductInfo = extract_product(document_tree)
+            extracted_info.base_url = page.base_url
+            storage_q.put(extracted_info)
+            with data_lock:
+                category: CategoryStatus = category_status[page.base_url]
+                completed_products = category.completed_products
+                category.completed_products = completed_products + 1
+                category_status[page.base_url] = category
+            log.debug(f'{category=}')
         log.debug(f'{product_count=}')
     log.debug('Exitting processor loop')
-
 
 
 def check_domain(url: ParseResult):
@@ -261,21 +344,32 @@ def load_categories(path: str) -> list[str]:
     f.close()
     return categories
 
-def category_is_completed(category: CategoryStatus):
-    return category.total_products == category.completed_products and category.total_search_pages == category.completed_search_pages
 
-def start(categories, num_pages: int, num_procs: int, f_output: str, q_timeout, *args, **kwargs):
-    print(f'Runnig with args {categories=}, {num_pages=}, {num_procs=}')
+def category_is_completed(category: CategoryStatus):
+    return (
+        category.total_products == category.completed_products and
+        category.total_search_pages == category.completed_search_pages and
+        category.saved_products == category.total_products
+    )
+
+
+def start(categories, num_pages: int, num_procs: int, output_links: str, output_details, q_timeout, *args, **kwargs):
+    log.debug(f'Runnig with args {categories=}, {num_pages=}, {num_procs=}')
+    f_links = open(output_links, 'w')
+    f_details = open(output_details, 'w')
+    f_links.write('[')
+    f_details.write('[')
     mgr = Manager()
     categories = load_categories(categories)
-    url_queue = Queue()
-    response_queue = Queue()
+    url_queue = mgr.Queue()
+    response_queue = mgr.Queue()
     stop_ev = mgr.Event()
-    category_fetch_completed = mgr.Event()
     fetchers = []
     processors = []
     category_status = mgr.dict()
     data_lock = mgr.Lock()
+    storage_queue = mgr.Queue()
+    storage_lock = mgr.Lock()
     for url in categories:
         page = Page(base_url=url, url=url,
                     page_type=PageType.SEARCH_PAGE, page_number=1)
@@ -284,20 +378,32 @@ def start(categories, num_pages: int, num_procs: int, f_output: str, q_timeout, 
         url_queue.put(page)
     for _ in range(num_procs):
         f = Process(target=fetcher, args=(
-            url_queue, response_queue, stop_ev, category_fetch_completed, q_timeout))
+            url_queue, response_queue, stop_ev, q_timeout))
         f.start()
         fetchers.append(f)
     for _ in range(num_procs):
         p = Process(target=processor, args=(response_queue, url_queue,
-                    q_timeout, stop_ev, category_status, category_fetch_completed, data_lock))
+                    q_timeout, stop_ev, category_status, data_lock, storage_queue))
         p.start()
         processors.append(p)
+    #links_storage_process = Process(target=None)
+    details_storage_process = Process(target=save_product_info, args=(
+        storage_queue, f_details, storage_lock, stop_ev, q_timeout, category_status, data_lock))
+    details_storage_process.start()
     while not stop_ev.is_set():
         sleep(5)
         data_lock.acquire()
-        print(f'MP {category_status}')
         if all(category_is_completed(category) for category in category_status.values()):
             stop_ev.set()
         data_lock.release()
+        log.debug(f'MP {category_status}')
     for p in chain(fetchers, processors):
         p.join()
+    details_storage_process.join()
+    # FIXME: remove this extra object and traling comma
+    storage_lock.acquire()
+    f_links.write('sadfsdfsadfasdfasdfa{}]')
+    f_details.write('lounoiunoiumn{}]')
+    storage_lock.release()
+    f_links.close()
+    f_details.close()
