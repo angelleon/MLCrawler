dd01e64b074233b5de3ea835610ec0b38346ad94
Angel Leon <luianglenlop@gmail.com>
2022-05-16 10:33:23 -0500

    now default format for saving is json

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -8,7 +8,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('-c', '--categories', help='file containing categories, one per line', default='categories.txt')
     parser.add_argument('-p', '--pages', help='number search of pages to fetch and analyze', type=int, default=10)
-    parser.add_argument('-o', '--output-links', help='file to write the search results on', default='product_links.csv')
+    parser.add_argument('-o', '--output-links', help='file to write the search results on', default='product_links.json')
     parser.add_argument('-O', '--output-details', help='file to write the results on', default='product_info.json')
     parser.add_argument('-n', '--num-procs', help='Number of processes', type=int, default=5)
     parser.add_argument('-t', '--timeout-q', help='Timeout when fetching from queues, in seconds', type=float, default=0.01)
